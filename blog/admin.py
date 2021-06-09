2740eb7a16baacc0c9322bb24e8ca20b3d871430
hichem chatbi <hichem@courrier.agency>
2021-06-09 23:58:01 +0200

    creation profile et tag sur l'interface admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from blog.models import Profile, Post, Tag
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
