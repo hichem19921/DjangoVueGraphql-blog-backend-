f0950b589e88ca72e584609e3f5b96047bf87b1d
hichem chatbi <hichem@courrier.agency>
2021-06-09 22:36:01 +0200

    creation user field

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
+class profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,)
+    
